0dbb4e1792663ed5fc23fb2520ad2436782e9c8f
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2021-10-21 08:13:59 +0200

    martinez

--- src/admin/pages/Admin.js
+++ src/admin/pages/Admin.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../shared/context/auth-context';
+import AuthModal from '../components/AuthModal';
 import EditAboutUs from '../components/EditAboutUs';
 import EditNews from '../components/EditNews';
 import EditOpening from '../components/EditOpening';
 
 const Admin = () => {
+    const { token, isLoggedIn } = useContext(AuthContext);
 
     return (
         <React.Fragment>
             <div className='admin'>
                 <h2>beállítások:</h2>
                 <div className='admin__inner-border'>
-  
-                        <EditOpening />
-                        <EditAboutUs />
-                        <EditNews />
-        
+
+                    <EditOpening />
+                    <EditAboutUs />
+                    <EditNews />
+
                     <div>
-                        <Link
-                            className='admin__menu'
-                            to='/admin/menu'
-                        >
-                            <span>Menü</span>
-                        </Link>
+                        <AuthModal>
+                            <Link
+                                className='admin__menu'
+                                to={isLoggedIn ? '/admin/menu' : undefined}
+                            >
+                                <span>Menü</span>
+                            </Link>
+                        </AuthModal>
+
                     </div>
 
                 </div>
