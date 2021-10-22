01497c41eb4611b1421ed5102e6a0ea2b01b94b1
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2021-10-22 19:41:03 +0200

    wip

---
+++ src/admin/components/MenuButton.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../shared/context/auth-context';
+import AuthModal from './AuthModal';
+
+export const MenuButton = (props) => {
+    const { token, isLoggedIn } = useContext(AuthContext);
+
+    return <div>
+        <AuthModal>
+            <Link
+                className='admin__menu cursor-pointer'
+                to={isLoggedIn ? '/admin/menu' : undefined}
+            >
+                <span>Menü</span>
+            </Link>
+        </AuthModal>
+
+    </div>
+
+
+}
