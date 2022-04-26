05b8c0b5c3e68185bc0f11e38612dca7a6d3b4f1
Zoltan Csilleri <zcsilleri@gmail.com>
2022-04-26 14:23:46 +0200

    wip

--- src/menu/components/FoodItem.js
+++ src/menu/components/FoodItem.js
@@ -12,28 +12,26 @@ export const FoodItem = (props) => {
         {types && <h2 className='heading-secondary'>{title}</h2>}
 
         {types && types.map(i => {
-            return (
-                <div
-                    key={i._id}
-                    className={`${location.pathname === '/admin/menu' ? 'menu-item-wrapper hover-opacity' : null}`}
-                    onClick={props.onClick}
-                >
-                    <div className='food-item'>
-                        <div className={'flex-basis-80'}>
-                            <p className='paragraph paragraph--menu'>{i.name}</p>
-                            <p className={''}>{i.description}</p>
-                        </div>
-                        <div className={'flex-basis-20'}>
-                            <p className='paragraph paragraph--menu white-space-nowrap flex-basis-30 text-align-right'>{i.price} Ft</p>
-                        </div>
-
-                        {location.pathname === '/admin/menu' && <div
-                            id={i.identifier}
-                            className={'menu-admin-view'}
-                        />}
+            return (<div
+                key={i._id}
+                className={`${location.pathname === '/admin/menu' ? 'menu-item-wrapper hover-opacity' : null}`}
+                onClick={props.onClick}
+            >
+                <div className='food-item'>
+                    <div className={'flex-basis-80'}>
+                        <p className='paragraph paragraph--menu'>{i.name}</p>
+                        <p className={''}>{i.description}</p>
                     </div>
+                    <div className={'flex-basis-20'}>
+                        <p className='paragraph paragraph--menu white-space-nowrap flex-basis-30 text-align-right'>{i.price} Ft</p>
+                    </div>
+
+                    {location.pathname === '/admin/menu' && <div
+                        id={i.identifier}
+                        className={'menu-admin-view'}
+                    />}
                 </div>
-            )
+            </div>)
         })}
 
     </div>;
