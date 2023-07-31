e4540f864e854507c12c5d68e2e24363af824a45
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2023-07-31 10:21:52 +0200

    wip

--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const { exec } = require('child_process');
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
+app.get('*', async (req, res) => {
+    await seeStructure();
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+async function seeStructure() {
+    return new Promise((resolve, reject) => {
+        exec('ls -la', (a, b, c) => console.log({
+            a, b, c, __dirname
+        }));
+    });
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
